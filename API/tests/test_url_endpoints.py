5257fba20530cd96d9b88a276cc90072be8b32d8
Dianawats <dianakiznaki@gmail.com>
2018-06-21 15:30:50 +0300

    add test case methods

--- API/tests/test_url_endpoints.py
+++ API/tests/test_url_endpoints.py
@@ -29,3 +29,19 @@ class Url_endpoints(unittest.TestCase):
     def tearDown(self):
         """ Teardown method for the unit tests """
         pass
+
+     def test_main_route(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_can_get_all_rides(self):
+        """ Test whether we can get all the rides """
+        response = self.app.get('api/v1/rides/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Bukoto', str(response.data))
+
+    def test_user_can_get_one_request_for_ride(self):
+        """ Test whether we can get all the rides """
+        response = self.app.get('api/v1/ride/1')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Fetch person X', str(response.data))
