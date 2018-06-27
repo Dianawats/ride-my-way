8faf08f002419d7f81ee32d68400ad7c8d19731f
Dianawats <dianakiznaki@gmail.com>
2018-06-27 19:25:39 +0300

    Adding more tests

--- API/tests/test_url_endpoints.py
+++ API/tests/test_url_endpoints.py
@@ -1,8 +1,10 @@
 import os
 import unittest
+from flask import json
 
 from API.app import app
 from API.models.model import Rides_db
+
 
 
 class Url_endpoints(unittest.TestCase):
@@ -20,15 +22,7 @@ class Url_endpoints(unittest.TestCase):
         self.ride.get_all()
         # self.ride = Rides_db()
         # self.ride.create({'task': 'Fetch person X', "name": "Car pool",
-                        #   "status": False, "destination": "ntinda"})
-
-        self.dummy_data = {
-
-            "details": "car carpool",
-            "name": "request car ride",
-            "status": False,
-            "destination": "I am going to kamwokya"
-        }
+                        #   "status": False, "destination": "ntinda"})        
 
     def tearDown(self):
         """ Teardown method for the unit tests """
@@ -42,26 +36,39 @@ class Url_endpoints(unittest.TestCase):
         """ Test whether we can get all the rides """
         response = self.app.get('api/v1/rides/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn('Bukoto', str(response.data))
+        self.assertIn('Ntinda', str(response.data))
 
     def test_user_can_get_one_request_for_ride(self):
         """ Test whether we can get a single ride details """
         response = self.app.get('api/v1/ride/1')
         self.assertEqual(response.status_code, 200)
-        self.assertIn('Fetch person X', str(response.data))
+        self.assertIn('Car pool', str(response.data))
 
 
     def test_driver_can_create_a_ride(self):
         """ Test whether we can create a ride """
-        response = self.app.post('api/v1/rides/', self.dummy_data)
+        dummy_data = {
+            "id":'0',
+            "details": "car carpool",
+            "name": "request car ride",
+            "status": False,
+            "destination": "I am going to kamwokya"
+        }
+        response = self.app.post('api/v1/rides/', data=json.dumps(dummy_data),
+                                    content_type='application/json',)
         self.assertEqual(response.status_code, 201)
         self.assertIn('I am going to kamwokya', str(response.data))
 
     def test_passenger_can_request_to_join_a_ride(self):
         """ Test whether we can create a ride """
-        response = self.app.post('api/v1/rides/1/request', {'id':'1', 'join_details':'I would like to join your ride'})
+        request_data = {
+            'id':'1',
+            'join_details':'I want to join your ride'
+        }
+        response = self.app.post('api/v1/ride/1/requests', data=json.dumps(request_data),
+                                    content_type='application/json')
         self.assertEqual(response.status_code, 201)
-        self.assertIn('I am going to kamwokya', str(response.data))
+        self.assertIn('I want to join your ride', str(response.data))
 
 
 if __name__ == '__main__':
