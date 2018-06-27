99f44f7a67b37ec10f2c998d32d6b438d8597553
Dianawats <dianakiznaki@gmail.com>
2018-06-27 18:38:41 +0300

    Adding more tests

--- API/tests/test_url_endpoints.py
+++ API/tests/test_url_endpoints.py
@@ -9,12 +9,15 @@ class Url_endpoints(unittest.TestCase):
     # setup and teardown of the app
 
     def setUp(self):
+        details = 'Fetch person X'
+        name = 'Car pool'
+        status = False
+        destination = 'Ntinda'
         app.config['TESTING'] = True
         app.config['DEBUG'] = False
         self.app = app.test_client()
-        self.ride = Rides_db()
-        self.ride.create({'task': 'Fetch person X', "name": "Car pool",
-             "status": False, "destination": "ntinda"})
+        self.ride = Rides_db(details, name, status, destination)
+        self.ride.get_all()
         # self.ride = Rides_db()
         # self.ride.create({'task': 'Fetch person X', "name": "Car pool",
                         #   "status": False, "destination": "ntinda"})
@@ -42,17 +45,23 @@ class Url_endpoints(unittest.TestCase):
         self.assertIn('Bukoto', str(response.data))
 
     def test_user_can_get_one_request_for_ride(self):
-        """ Test whether we can get all the rides """
+        """ Test whether we can get a single ride details """
         response = self.app.get('api/v1/ride/1')
         self.assertEqual(response.status_code, 200)
         self.assertIn('Fetch person X', str(response.data))
 
 
-    # def test_driver_can_create_a_ride(self):
-    #     """ Test whether we can get all the rides """
-    #     response = self.app.post('api/v1/rides/', self.dummy_data)
-    #     self.assertEqual(response.status_code, 201)
-    #     self.assertIn('I am going to kamwokya', str(response.data))
+    def test_driver_can_create_a_ride(self):
+        """ Test whether we can create a ride """
+        response = self.app.post('api/v1/rides/', self.dummy_data)
+        self.assertEqual(response.status_code, 201)
+        self.assertIn('I am going to kamwokya', str(response.data))
+
+    def test_passenger_can_request_to_join_a_ride(self):
+        """ Test whether we can create a ride """
+        response = self.app.post('api/v1/rides/1/request', {'id':'1', 'join_details':'I would like to join your ride'})
+        self.assertEqual(response.status_code, 201)
+        self.assertIn('I am going to kamwokya', str(response.data))
 
 
 if __name__ == '__main__':
